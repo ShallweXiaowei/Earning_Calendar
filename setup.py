bffccd6a5d0f56e09f195ab825bdeeee12f35d47
Xiaowei Yan <shufeyxw@gmail.com>
2017-01-21 16:15:54 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+from distutils.core import setup
+
+
+setup(
+    name = 'Earning_Calendar',
+    version = "1.0.0",
+    author = 'Xiaowei Yan',
+    author_email = 'shufeyxw@gmail.com',
+    description = ("This module is used to scarp earning information from both yahoo and nasdaq"),
+    license = "MIT",
+    url = "https://github.com/ShallweXiaowei/Earning_Calendar",
+    download_url = "https://github.com/ShallweXiaowei/Earning_Calendar/archive/1.0.tar.gz",
+    packages = ['Earning_Calendar']
+)
